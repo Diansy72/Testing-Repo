3751ea67739395c4f3af3dcfd76afc9c6b690d95
Diansy72 <email-nurferdiansaheka@gmail.com>
2025-03-16 15:37:28 +0700

    Added getGreeting function to provide time based greetings

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ console.log('Hello world');
 console.log('Today date:', new Date().toLocaleDateString());
 function getGreeting() {
     const hour = new Date().getHours();
-    if (hour < 12) return 'Selamat Pagi!';
-    if (hour < 18) return 'Selamat Siang!';
-    return 'Selamat Malam!';
+    if (hour < 12) return 'Good morning!';
+    if (hour < 18) return 'Good afternoon!';
+    return 'Good evening!';
 }
 console.log(getGreeting());
